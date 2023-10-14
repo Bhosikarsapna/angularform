ec8fab662e6f316e3acaf5f6320d57085ffb505c
Bhosikarsapna <124525656+Bhosikarsapna@users.noreply.github.com>
2023-10-15 00:39:06 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularform';
+
+  contactMode = [
+    {moc : 'Email', id:1},
+    {moc : 'Phone', id:2}
+
+  ]
+
+  onsignUp(signUp : NgForm){
+   if(signUp.valid){
+    console.log(signUp);
+    console.log(signUp.value);
+    signUp.reset()
+   }else{
+    return
+   }
+  }
 }
